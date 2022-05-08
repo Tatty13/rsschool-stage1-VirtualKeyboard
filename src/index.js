9985d6024bc578bdbba50c0bcc918afa54c70e71
Tatty13 <tat.mi.iva@gmail.com>
2022-05-08 19:06:17 +0300

    feat: add text-area to the body

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './css/main.css'
+
+const body = document.querySelector('body');
+const textArea = document.createElement("textarea");
+
+body.appendChild(textArea);
+textArea.rows = '10';
+textArea.cols = '100';
+
+//body.innerHTML = keyBoardSymbols;
